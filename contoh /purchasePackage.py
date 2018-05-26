d30e1b96cc41d2b506733c4f5bba45652a9a3b02
Rizkyaa <36701336+Rizkyaa@users.noreply.github.com>
2018-05-26 20:27:21 +0700

    Delete purchasePackage.py

--- contoh /purchasePackage.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from xlpy import *
-
-xl = XL('MSISDN/NO.TELP')
-r = xl.loginWithOTP('OTP Code')
-if(r != False):
-    print(xl.purchasePackage('Service ID')['message'])
-else:
-    print("Login failed try again")
