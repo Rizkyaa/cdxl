05b2da802c90cf41cbee14a89541677bc6315282
Rizkyaa <36701336+Rizkyaa@users.noreply.github.com>
2018-05-26 20:18:53 +0700

    Create __init__.py

---
+++ xlpy/__init__.py
@@ -0,0 +1,10 @@
+from .config import Config
+from .client import XL
+
+__copyright__       = 'Copyright 2018 by Rizky'
+__version__         = '1.0.0'
+__author__          = 'Rizky'
+__author_email__    = 'rizkyardika123@gmail.com'
+__url__             = 'https://github.com/Rizkyaa/cdxl'
+
+__all__ = ['Config','XL']
